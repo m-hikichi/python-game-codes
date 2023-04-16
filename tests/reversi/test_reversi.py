5a82ea0f6ae6a71b12458c7da36103599b092ec2
m_hikichi <hijiki.mshr19980119@outlook.com>
2023-04-16 13:12:53 +0900

    test: reversi.pyが想定通りに動作しているかどうかを確認するためのtest_reversi.pyの追加

---
+++ tests/reversi/test_reversi.py
@@ -0,0 +1,57 @@
+import pytest
+from src.reversi.reversi import Reversi
+
+
+@pytest.mark.parametrize(
+    "input, ans",
+    [
+        (Reversi.Stone.BLACK, Reversi.Stone.WHITE),
+        (Reversi.Stone.WHITE, Reversi.Stone.BLACK),
+        (Reversi.Stone.EMPTY, None),
+    ],
+)
+def test_opposite_stone(input, ans):
+    # GIVEN
+
+    # WHEN
+    ret = Reversi.Stone.opposite(input)
+
+    # THEN
+    assert ret == ans
+
+
+@pytest.fixture()
+def init_reversi():
+    return Reversi()
+
+
+def test_init_reverisi(init_reversi):
+    # GIVEN
+
+    # WHEN
+    reversi = init_reversi
+
+    # THEN
+    assert reversi.count_stone(Reversi.Stone.BLACK) == 2
+    assert reversi.count_stone(Reversi.Stone.WHITE) == 2
+    assert reversi.get_stone(3, 3) == Reversi.Stone.BLACK
+    assert reversi.get_stone(4, 4) == Reversi.Stone.BLACK
+    assert reversi.get_stone(3, 4) == Reversi.Stone.WHITE
+    assert reversi.get_stone(4, 3) == Reversi.Stone.WHITE
+
+
+def test_put_stone(init_reversi):
+    # GIVEN
+    reversi = init_reversi
+
+    # WHEN
+    reversi.put_stone(2, 4, Reversi.Stone.BLACK)
+
+    # THEN
+    assert reversi.count_stone(Reversi.Stone.BLACK) == 4
+    assert reversi.count_stone(Reversi.Stone.WHITE) == 1
+    assert reversi.get_stone(3, 3) == Reversi.Stone.BLACK
+    assert reversi.get_stone(4, 4) == Reversi.Stone.BLACK
+    assert reversi.get_stone(3, 4) == Reversi.Stone.BLACK
+    assert reversi.get_stone(2, 4) == Reversi.Stone.BLACK
+    assert reversi.get_stone(4, 3) == Reversi.Stone.WHITE
