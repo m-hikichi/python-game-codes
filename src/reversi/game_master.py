0d0ecb57a66e1aa9aa8f1f37ebb392c666dced4b
m_hikichi <hijiki.mshr19980119@outlook.com>
2023-04-30 09:15:37 +0900

    fix: 対戦するプレイヤーの石の色が黒と白以外の組み合わせでも初期化できていたバグの修正，およびReversiGameMasterクラスのメンバ変数をPrivate修飾子に変更

--- src/reversi/game_master.py
+++ src/reversi/game_master.py
@@ -5,29 +5,38 @@ from typing import Optional
 
 class ReversiGameMaster():
     def __init__(self, black_player: ReversiPlayer, white_player: ReversiPlayer):
-        self.reversi_board = ReversiBoard()
-        self.players = {
+        self.__reversi_board = ReversiBoard()
+
+        if black_player.get_stone_color() != ReversiBoard.Stone.BLACK:
+            raise InvalidStoneColorError("Black player must have BLACK stone color.")
+        if white_player.get_stone_color() != ReversiBoard.Stone.WHITE:
+            raise InvalidStoneColorError("White player must have WHITE stone color")
+        self.__players = {
             ReversiBoard.Stone.BLACK: black_player,
             ReversiBoard.Stone.WHITE: white_player
         }
-        self.current_player_color = ReversiBoard.Stone.BLACK
+        self.__current_player_color = ReversiBoard.Stone.BLACK
 
     def play_game(self) -> None:
-        while not self.reversi_board.is_game_over():
-            current_player = self.players[self.current_player_color]
-            current_player.play(self.reversi_board)
-            print(self.reversi_board)
-            self.current_player_color = ReversiBoard.Stone.opposite(self.current_player_color)
+        while not self.__reversi_board.is_game_over():
+            current_player = self.__players[self.__current_player_color]
+            current_player.play(self.__reversi_board)
+            print(self.__reversi_board)
+            self.__current_player_color = ReversiBoard.Stone.opposite(self.__current_player_color)
 
     def get_winner(self) -> Optional[ReversiBoard.Stone]:
-        black_count = self.reversi_board.count_stone(ReversiBoard.Stone.BLACK)
-        white_count = self.reversi_board.count_stone(ReversiBoard.Stone.WHITE)
+        black_count = self.__reversi_board.count_stone(ReversiBoard.Stone.BLACK)
+        white_count = self.__reversi_board.count_stone(ReversiBoard.Stone.WHITE)
         if black_count > white_count:
             return ReversiBoard.Stone.BLACK
         elif black_count < white_count:
             return ReversiBoard.Stone.WHITE
         else:
             return None
+
+
+class InvalidStoneColorError(Exception):
+    pass
 
 
 if __name__=="__main__":
