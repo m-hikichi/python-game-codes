2e7b18c4606e0092d470fb8444e812606c377c11
m_hikichi <hijiki.mshr19980119@outlook.com>
2023-11-12 17:07:06 +0900

    feat: 二分木によるダンジョンマップの作成

--- src/dungeon/dungeon.py
+++ src/dungeon/dungeon.py
@@ -29,12 +29,33 @@ class Dungeon:
         return dungeon_representation
 
     def __create_rooms(self):
-        def split_map(sub_map):
-            @unique
-            class SplitDirection(Enum):
-                HORIZON = "Horizon"
-                VERTICAL = "Vertical"
+        class Node:
+            def __init__(self):
+                self.left_node = None
+                self.right_node = None
+                self.boundary_line = None
+                self.split_direction = None
+                self.sub_map = None
 
+        class BinaryTree:
+            def __init__(self):
+                self.root = None
+
+            @staticmethod
+            def get_leaf_node(node):
+                if node.left_node is not None:
+                    yield from BinaryTree.get_leaf_node(node.left_node)
+                if node.right_node is not None:
+                    yield from BinaryTree.get_leaf_node(node.right_node)
+                if node.left_node is None and node.right_node is None:
+                    yield node
+
+        @unique
+        class SplitDirection(Enum):
+            HORIZON = "Horizon"
+            VERTICAL = "Vertical"
+
+        def split_map(sub_map):
             split_directions = []
             sub_map_height, sub_map_width = sub_map.shape
             if sub_map_width > (Dungeon.MIN_ROOM_WIDTH+(Dungeon.ROOM_MARGIN*2))*2+1:
@@ -42,21 +63,29 @@ class Dungeon:
             if sub_map_height > (Dungeon.MIN_ROOM_HEIGHT+(Dungeon.ROOM_MARGIN*2))*2+1:
                 split_directions.append(SplitDirection.VERTICAL)
 
+            node = Node()
+            node.sub_map = sub_map
+
             if len(split_directions) == 0:
-                yield sub_map
-                return
+                return node
 
             split_direction = random.choice(split_directions)
             if split_direction == SplitDirection.HORIZON:
                 split_x = random.randint(Dungeon.MIN_ROOM_WIDTH+(Dungeon.ROOM_MARGIN*2)+1, sub_map_width-(Dungeon.MIN_ROOM_WIDTH+(Dungeon.ROOM_MARGIN*2)+1))
-                yield from split_map(sub_map[:, 0:split_x])
-                yield from split_map(sub_map[:, split_x+1:sub_map_width])
-                sub_map[:, split_x] = Dungeon.Area.LOAD
+                node.boundary_line = split_x
+                node.split_direction = split_direction
+                node.left_node = split_map(sub_map[:, 0:split_x])
+                node.right_node = split_map(sub_map[:, split_x+1:sub_map_width])
+                node.sub_map[:, split_x] = Dungeon.Area.LOAD
+                return node
             if split_direction == SplitDirection.VERTICAL:
                 split_y = random.randint(Dungeon.MIN_ROOM_HEIGHT+(Dungeon.ROOM_MARGIN*2)+1, sub_map_height-(Dungeon.MIN_ROOM_HEIGHT+(Dungeon.ROOM_MARGIN*2)+1))
-                yield from split_map(sub_map[0:split_y, :])
-                yield from split_map(sub_map[split_y+1:sub_map_height, :])
-                sub_map[split_y, :] = Dungeon.Area.LOAD
+                node.boundary_line = split_y
+                node.split_direction = split_direction
+                node.left_node = split_map(sub_map[0:split_y, :])
+                node.right_node = split_map(sub_map[split_y+1:sub_map_height, :])
+                node.sub_map[split_y, :] = Dungeon.Area.LOAD
+                return node
 
         def create_room(sub_map):
             sub_map_height, sub_map_width = sub_map.shape
@@ -71,6 +100,7 @@ class Dungeon:
 
             sub_map[room_left_upper_y:room_left_upper_y+room_height, room_left_upper_x:room_left_upper_x+room_width] = Dungeon.Area.ROOM
 
-        sub_maps = split_map(self.__dungeon_map)
-        for sub_map in sub_maps:
-            create_room(sub_map)
+        binary_tree_map = BinaryTree()
+        binary_tree_map.root = split_map(self.__dungeon_map)
+        for node in BinaryTree.get_leaf_node(binary_tree_map.root):
+            create_room(node.sub_map)
